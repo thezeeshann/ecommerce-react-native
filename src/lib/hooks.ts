56d7ae6cc85d1e2584b5ea680cf92efba9ff8a71
thezeeshann <info.zeeshann@gmail.com>
2025-04-11 13:00:47 +0530

    added app context hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../context/cart-context";
+import { AppContext } from "../context/app-context";
 
 export function useDebounce(value: string): string {
   const [debouncedValue, setDebouncedValue] = useState(value);
@@ -17,9 +18,15 @@ export function useDebounce(value: string): string {
 export function useCartContext() {
   const context = useContext(CartContext);
   if (!context) {
-    throw new Error(
-      "useCartContext must be used within a CartContextProvider"
-    );
+    throw new Error("useCartContext must be used within a CartContextProvider");
+  }
+  return context;
+}
+
+export function useAppContext() {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext must be used within a AppContextProvider");
   }
   return context;
 }
